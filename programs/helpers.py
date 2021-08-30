1150289abe6cc646b16df588629fe0443d3072af
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2021-08-30 11:02:04 +0200

    redaction of comparison notebooks

---
+++ programs/helpers.py
@@ -0,0 +1,22 @@
+def show(T, F, ws, getBHSinfo, getOSMinfo, indent='', asString=False):
+    '''
+    We need to show cases.
+    Prints information about the word nodes in list `ws`.
+    Which information is printed, depends on the two other arguments,
+    which are functions, taking a word node
+    and delivering information about that word.
+    getBHSInfo will be used to grab BHS info, getOSMinfo will be used to grab OSM info.
+    '''
+    t = ('{}{} w{}"{}"\n{}\tBHS: {}\n{}\tOSM: {}'.format(
+        indent,
+        '{} {}:{}'.format(*T.sectionFromNode(ws[0])),
+        '/'.join(str(w) for w in ws),
+        '/'.join(F.g_word_utf8.v(w) for w in ws),
+        indent,
+        '/'.join(getBHSinfo(w) for w in ws),
+        indent,
+        '/'.join(getOSMinfo(w) for w in ws),
+    ))
+    if asString:
+        return t
+    print(t)
